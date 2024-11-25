9bcc86d692d3b61f912b7fe1b72c6fe8c32370a1
portfolio_dimas <dimasbintangpratama5@gmail.com>
2024-11-25 16:31:46 +0700

    fix : Update Page And Timeline in OtherCostDetailPage, PurchaseDetailPage

--- src/Pages/Pembelian/OtherCostDetailPage.tsx
+++ src/Pages/Pembelian/OtherCostDetailPage.tsx
@@ -1,7 +1,8 @@
-import Breadcrumb from "../../Components/Breadcrumb";
+import Breadcrumb from "../../Components/Breadcrumb"; // Pastikan komponen ini ada
 import LayoutProject from "../../Layouts/layoutProject";
-
 import PurchaseDetailLayout from "../../Layouts/PurchaseDetailLayout";
+import { FaRegFileAlt } from "react-icons/fa";
+import TimelineApproval from "../../Components/pembelian/TimelineApproval";
 
 const breadcrumbItems = [
   { label: "Home", link: "/" },
@@ -10,17 +11,108 @@ const breadcrumbItems = [
   { label: "Detail Pembelian" },
 ];
 
-const OtherCostDetailPage = () => {
+const PaymentInfoPage = () => {
   return (
-    <>
+    <div className="min-h-screen">
+      {/* Breadcrumb Component */}
       <Breadcrumb title="Pembelian" items={breadcrumbItems} />
+
       <LayoutProject bgColor="bg-transparent">
         <PurchaseDetailLayout>
-          <h1>rincian biaya lainnya</h1>
+          {/* Vendor Info */}
+          <div className="my-4 flex justify-between">
+            <div>
+              <p className="text-slate-400">Nama Vendor</p>
+              <b className="font-bold">PT Charoen Pokphand Indonesia Tbk</b>
+            </div>
+            <div className="text-right">
+              <p className="text-slate-400">Alamat</p>
+              <div className="font-bold">
+                Jl. Ancol VIII/1, Ancol, Pademangan
+                <br /> North Jakarta, DKI Jakarta, 14430
+              </div>
+            </div>
+          </div>
+
+          {/* No. PR, No. PO, and Tanggal Dibutuhkan with Border */}
+
+          <div className="my-4 flex justify-between gap-4 items-center border p-4 rounded-lg">
+            <div className="flex gap-4 items-center">
+              {/* No. PR */}
+              <div className="flex items-center">
+                <p className="text-sm font-medium mr-2">No. PR:</p>
+                <span className="font-semibold text-blue-400">
+                  PR-MAN-00005
+                </span>
+              </div>
+
+              {/* No. PO */}
+              <div className="flex items-center">
+                <p className="text-sm font-medium mr-2">No. PO:</p>
+                <span className="font-semibold text-blue-400">
+                  PO-MAN-00005
+                </span>
+              </div>
+            </div>
+
+            {/* Tanggal Dibutuhkan */}
+            <div className="flex items-center gap-4">
+              <p className="text-sm font-medium whitespace-nowrap text-slate-400">
+                Tanggal Dibutuhkan:
+              </p>
+              <input type="date" className="input input-bordered bg-blue-200" />
+            </div>
+          </div>
+
+          {/* Payment Info Table */}
+          <div className="my-4 overflow-x-auto">
+            <h3>Info Pembayaran</h3>
+            <table className="table w-full table-zebra">
+              <thead className="bg-blue-200">
+                <tr className="text-center">
+                  <th>#</th>
+                  <th>Nama Biaya</th>
+                  <th>Nominal</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="text-center">
+                  <td>1</td>
+                  <td>xxxxxx</td>
+                  <td>xxxxxx</td>         
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          {/* Total Pembayaran */}
+
+          <div className="my-4 p-4 border rounded-lg bg-gray-100 flex justify-end">
+            <div>
+              <div className="flex justify-between items-center mb-2 gap-6">
+                <p className="text-sm font-medium text-gray-600">
+                  Total Biaya Lainnya:{" "}
+                </p>
+                <span className="text-sm font-semibold text-gray-800">
+                  0.00
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <TimelineApproval currentApprovalStep={3} />
+
+          {/* Action Buttons */}
+          <div className="my-4 flex justify-end gap-4">
+            <button className="btn bg-orange-500 text-white w-32 hover:bg-orange-600">
+              Tolak
+            </button>
+            <button className="btn btn-success w-32 text-white">Setujui</button>
+          </div>
         </PurchaseDetailLayout>
       </LayoutProject>
-    </>
+    </div>
   );
 };
 
-export default OtherCostDetailPage;
+export default PaymentInfoPage;
--- src/Pages/Pembelian/PurchaseDetailPage.tsx
+++ src/Pages/Pembelian/PurchaseDetailPage.tsx
@@ -1,6 +1,6 @@
+import { useState } from "react";
 import Breadcrumb from "../../Components/Breadcrumb";
 import LayoutProject from "../../Layouts/layoutProject";
-
 import PurchaseDetailLayout from "../../Layouts/PurchaseDetailLayout";
 import TimelineApproval from "../../Components/pembelian/TimelineApproval";
 
@@ -12,143 +12,205 @@ const breadcrumbItems = [
 ];
 
 const PurchaseDetailPage = () => {
+  const [isRejectModalOpen, setRejectModalOpen] = useState(false);
+  const [comment, setComment] = useState("");
+
+  const handleReject = () => {
+    console.log("Komentar Ditolak:", comment);
+    setRejectModalOpen(false);
+    setComment(""); // Reset komentar setelah menyimpan
+  };
+
   return (
-    <>
+    <div className="min-h-screen">
+      {/* Breadcrumb */}
       <Breadcrumb title="Pembelian" items={breadcrumbItems} />
+
       <LayoutProject bgColor="bg-transparent">
         <PurchaseDetailLayout>
-          {/* Vendor Details */}
-          <div className="grid grid-cols-2 gap-4 mb-6">
+          {/* Vendor Info */}
+          <div className="my-4 flex justify-between">
             <div>
-              <p className="font-bold">Nama Vendor</p>
-              <p>PT Charoen Pokphand Indonesia Tbk</p>
+              <p className="text-slate-400">Nama Vendor</p>
+              <b className="font-bold">PT Charoen Pokphand Indonesia Tbk</b>
             </div>
-            <div>
-              <p className="font-bold">Alamat</p>
-              <p>
-                Jl. Ancol VIII/1, Ancol, Pademangan, North Jakarta, DKI Jakarta,
-                14430
-              </p>
-            </div>
-            <div>
-              <p className="font-bold">No. PR</p>
-              <p className="text-blue-500">PR-MAN-00005</p>
-            </div>
-            <div>
-              <p className="font-bold">No. PO</p>
-              <p className="text-blue-500">PR-MAN-00005</p>
-            </div>
-            <div>
-              <p className="font-bold">Tanggal Dibutuhkan</p>
-              <input
-                type="date"
-                className="border border-gray-300 rounded-md p-2 w-full"
-              />
+            <div className="text-right">
+              <p className="text-slate-400">Alamat</p>
+              <div className="font-bold">
+                Jl. Ancol VIII/1, Ancol, Pademangan
+                <br /> North Jakarta, DKI Jakarta, 14430
+              </div>
             </div>
           </div>
 
-          {/* List Item Table */}
-          <div className="overflow-auto">
-            <table className="w-full border-collapse border border-gray-300">
-              <thead className="bg-gray-100">
-                <tr>
-                  <th className="border border-gray-300 px-4 py-2">#</th>
-                  <th className="border border-gray-300 px-4 py-2">Produk</th>
-                  <th className="border border-gray-300 px-4 py-2">
-                    Jenis Produk
-                  </th>
-                  <th className="border border-gray-300 px-4 py-2">
-                    Gudang/Tempat Pengiriman
-                  </th>
-                  <th className="border border-gray-300 px-4 py-2">Jumlah</th>
-                  <th className="border border-gray-300 px-4 py-2">Satuan</th>
-                  <th className="border border-gray-300 px-4 py-2">
-                    Harga Satuan (Rp)
-                  </th>
-                  <th className="border border-gray-300 px-4 py-2">Pajak</th>
-                  <th className="border border-gray-300 px-4 py-2">
-                    Total (Rp)
-                  </th>
+          {/* No. PR, No. PO, and Tanggal Dibutuhkan */}
+          <div className="my-4 flex justify-between gap-4 items-center border p-4 rounded-lg">
+            <div className="flex gap-4 items-center">
+              <div className="flex items-center">
+                <p className="text-sm font-medium mr-2">No. PR:</p>
+                <span className="font-semibold text-blue-400">
+                  PR-MAN-00005
+                </span>
+              </div>
+              <div className="flex items-center">
+                <p className="text-sm font-medium mr-2">No. PO:</p>
+                <span className="font-semibold text-blue-400">
+                  PO-MAN-00005
+                </span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <p className="text-sm font-medium whitespace-nowrap text-slate-400">
+                Tanggal Dibutuhkan:
+              </p>
+              <input type="date" className="input input-bordered bg-blue-200" />
+            </div>
+          </div>
+
+          {/* Payment Info Table */}
+          <div className="my-4 overflow-x-auto">
+            <h3 className="text-lg font-bold mb-2">Info Pembayaran</h3>
+            <table className="table w-full border border-gray-300 rounded-lg">
+              <thead className="bg-blue-200">
+                <tr className="text-center">
+                  <th className="p-2">#</th>
+                  <th className="p-2">Produk</th>
+                  <th className="p-2">Jenis Produk</th>
+                  <th className="p-2">Gudang/Tempat Pengiriman</th>
+                  <th className="p-2">Jumlah</th>
+                  <th className="p-2">Satuan</th>
+                  <th className="p-2">Harga Satuan(Rp)</th>
+                  <th className="p-2">Pajak</th>
+                  <th className="p-2">Total(Rp)</th>
+
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td className="border border-gray-300 px-4 py-2">1</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    DOC CP Vaksin
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">Sapronak</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    Gudang Pangandaran
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">50,000</td>
-                  <td className="border border-gray-300 px-4 py-2">Ekor</td>
-                  <td className="border border-gray-300 px-4 py-2">7,000.00</td>
-                  <td className="border border-gray-300 px-4 py-2">11%</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    350,000,000.00
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-gray-300 px-4 py-2">2</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    Pakan Starter
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">Sapronak</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    Gudang Pangandaran
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">1,000</td>
-                  <td className="border border-gray-300 px-4 py-2">Kg</td>
-                  <td className="border border-gray-300 px-4 py-2">500.00</td>
-                  <td className="border border-gray-300 px-4 py-2">11%</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    500,000.00
-                  </td>
+                <tr className="text-center">
+                  <td className="p-2">1</td>
+                  <td className="p-2">Contoh Biaya</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
+                  <td className="p-2">Rp 10,000,000</td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          {/* Summary */}
-          <div className="mt-6 flex flex-col gap-2">
-            <div className="flex justify-between">
-              <p>Total Sebelum Pajak:</p>
-              <p>350,500,000.00</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Pajak:</p>
-              <p>38,555,000.00</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Diskon:</p>
-              <p>-0.00</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Total Biaya Lainnya:</p>
-              <p>0.00</p>
-            </div>
-            <div className="flex justify-between font-bold text-lg">
-              <p>Total:</p>
-              <p>389,055,000.00</p>
+          {/* Total Pembayaran */}
+          <div className="my-4 p-4 border rounded-lg bg-gray-100 flex justify-end">
+            <div>
+              <div className="flex justify-between items-center mb-2 gap-6">
+                <p className="text-sm font-medium text-gray-600">
+                  Total Pajak:{" "}
+                </p>
+                <span className="text-sm font-semibold text-gray-800">
+                  Rp 10,000,000
+                </span>
+              </div>
+              <div className="flex justify-between items-center mb-2 gap-6">
+                <p className="text-sm font-medium text-gray-600">
+                  Pajak:{" "}
+                </p>
+                <span className="text-sm font-semibold text-gray-800">
+                  Rp 12,000,000
+                </span>
+              </div>
+              <div className="flex justify-between items-center mb-2 gap-6">
+                <p className="text-sm font-medium text-gray-600">
+                  Diskon:{" "}
+                </p>
+                <span className="text-sm font-semibold text-gray-800">
+                  Rp 12,000,000
+                </span>
+              </div>
+              <div className="flex justify-between items-center mb-2 gap-6">
+                <p className="text-sm font-medium text-gray-600">
+                  Total Biaya Lainnya:{" "}
+                </p>
+                <span className="text-sm font-semibold text-gray-800">
+                  Rp 12,000,000
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="text-sm font-bold text-gray-800 mb-2 gap-6">
+                  Total:{" "}
+                </p>
+                <span className="text-sm font-bold text-gray-800">
+                  Rp 2,000,000
+                </span>
+              </div>
             </div>
           </div>
 
+          {/* Timeline Approval */}
           <TimelineApproval currentApprovalStep={3} />
 
-          {/* Buttons::START */}
-          <div className="mt-6 flex gap-4">
-            <button className="bg-red-500 text-white py-2 px-4 rounded-md">
+          {/* Action Buttons */}
+          <div className="my-4 flex justify-end gap-4">
+            <button
+              className="btn bg-orange-500 text-white w-32 hover:bg-orange-600"
+              onClick={() => setRejectModalOpen(true)}
+            >
               Tolak
             </button>
-            <button className="bg-green-500 text-white py-2 px-4 rounded-md">
-              Setuju
-            </button>
+            <button className="btn btn-success w-32 text-white">Setujui</button>
           </div>
-          {/* Buttons::END */}
         </PurchaseDetailLayout>
       </LayoutProject>
-    </>
+
+      {/* Reject Modal */}
+      {isRejectModalOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg w-96 relative">
+            {/* Close Button */}
+            <button
+              className="absolute top-2 right-2 text-xl font-bold text-gray-600"
+              onClick={() => setRejectModalOpen(false)}
+            >
+              &times;
+            </button>
+            <h3 className="text-lg font-bold text-blue-600">
+              Anda yakin memilih opsi Tolak?
+            </h3>
+            <p className="text-sm text-gray-500 mt-2">
+              Tolong berikan komentar/catatan jika Anda menolak
+            </p>
+            <textarea
+              className="w-full mt-4 p-2 border rounded-lg"
+              rows={4}
+              placeholder="Komentar/Catatan"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <div className="flex justify-end gap-4 mt-4">
+              <button
+                className="btn bg-gray-400 text-white hover:bg-gray-500"
+                onClick={() => setRejectModalOpen(false)}
+              >
+                Batal
+              </button>
+              <button
+                className={`btn ${
+                  comment.trim()
+                    ? "bg-blue-500 hover:bg-blue-600"
+                    : "bg-gray-300 cursor-not-allowed"
+                } text-white`}
+                onClick={handleReject}
+                disabled={!comment.trim()} // Disabled jika komentar kosong
+              >
+                Simpan
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
